ee3194c0ef652c1a13248aba66dedb6f08f1d7a8
Piotr <piotr.wolski@telecom-paristech.fr>
2019-02-07 00:21:13 -0500

    Parse args and compute motor moves

--- arduino/chess.ino
+++ arduino/chess.ino
@@ -4,17 +4,32 @@
 const float STEPS_PER_REV = 32;
 const float GEAR_RED = 64;
 const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;
+const float REV_PER_CM = 0.5;
+const float maxSpeed = 700;
 
 //Pins entered in sequence 1-3-2-4
 Stepper stepperMotor(STEPS_PER_REV, 8, 10, 9, 11);
 char inData[20];
+// Position in cm
+float posX;
+float posY;
 
 void setup()
 {
     Serial.begin(9600);
+    posX = 0;
+    posY = 0;
 }
 
-void receiveInstruction()
+float extractNumber(char *data, int n, int *end)
+{
+    *end = 0;
+    while (*end < n && data[*end] != '/' && data[*end] != '\0') (*end)++;
+    data[*end] = '\0';
+    return atof(data);
+}
+
+void receivePosition(float *x, float *y)
 {
     char inChar = '0';
     int index = 0;
@@ -27,14 +42,47 @@ void receiveInstruction()
         }
     } while (inChar != '\n' && index < 20);
     inData[index-1] = '\0';
+    int end;
     Serial.println(inData);
+    *x = extractNumber(inData, index, &end);
+    *y = extractNumber(inData+end+1, index, &end);
 }
 
 int incomingByte = 0;
 
+void computeMove(float x, float y, float *moveX, float *moveY)
+{
+    *moveX = x - posX;
+    *moveY = y - posY;
+}
+
+void move(float x, float y)
+{
+    float m = max(abs(x), abs(y));
+    if (m == 0)
+    {
+        Serial.println("Not moving");
+        return;
+    }
+    long speedX = long(maxSpeed * abs(x) / m);
+    long speedY = long(maxSpeed * abs(y) / m);
+    int stepsX = int(x*REV_PER_CM*STEPS_PER_OUT_REV);
+    int stepsY = int(y*REV_PER_CM*STEPS_PER_OUT_REV);
+}
+
 void loop()
 {
-    receiveInstruction();
+    float x,y;
+    receivePosition(&x, &y);
+    float moveX, moveY;
+    computeMove(x, y, &moveX, &moveY);
+    //Move
+    posX = x;
+    posY = y;
+    //Serial.print("X is ");
+    //Serial.println(x);
+    //Serial.print("Y is ");
+    //Serial.println(y);
     //int stepsRequired = STEPS_PER_OUT_REV;
     //stepperMotor.setSpeed(800);
     //stepperMotor.step(stepsRequired);
