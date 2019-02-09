05e8d3e5bffffcc69a618b0d9d512a4884ae6cba
Piotr <piotr.wolski@telecom-paristech.fr>
2019-02-09 18:29:28 -0500

    Use accelstepper library

--- arduino/chess.ino
+++ arduino/chess.ino
@@ -1,24 +1,29 @@
 #include "Stepper.h"
 #include "Arduino.h"
+#include "AccelStepper.h"
+#include "MultiStepper.h"
 
 const float STEPS_PER_REV = 32;
 const float GEAR_RED = 64;
 const float STEPS_PER_OUT_REV = STEPS_PER_REV * GEAR_RED;
 const float REV_PER_CM = 0.5;
+const float STEPS_PER_CM = REV_PER_CM * STEPS_PER_OUT_REV;
 const float maxSpeed = 700;
 
 //Pins entered in sequence 1-3-2-4
-Stepper stepperMotor(STEPS_PER_REV, 8, 10, 9, 11);
+AccelStepper stepperMotorY(AccelStepper::BYJ, 8, 9, 10, 11);
+AccelStepper stepperMotorX(AccelStepper::BYJ, 4, 5, 6, 7);
+MultiStepper steppers;
+
 char inData[20];
-// Position in cm
-float posX;
-float posY;
 
 void setup()
 {
     Serial.begin(9600);
-    posX = 0;
-    posY = 0;
+    stepperMotorX.setMaxSpeed(700);
+    stepperMotorY.setMaxSpeed(700);
+    steppers.addStepper(stepperMotorX);
+    steppers.addStepper(stepperMotorY);
 }
 
 float extractNumber(char *data, int n, int *end)
@@ -50,46 +55,24 @@ void receivePosition(float *x, float *y)
 
 int incomingByte = 0;
 
-void computeMove(float x, float y, float *moveX, float *moveY)
-{
-    *moveX = x - posX;
-    *moveY = y - posY;
-}
-
 void move(float x, float y)
 {
-    float m = max(abs(x), abs(y));
-    if (m == 0)
-    {
-        Serial.println("Not moving");
-        return;
-    }
-    long speedX = long(maxSpeed * abs(x) / m);
-    long speedY = long(maxSpeed * abs(y) / m);
-    int stepsX = int(x*REV_PER_CM*STEPS_PER_OUT_REV);
-    int stepsY = int(y*REV_PER_CM*STEPS_PER_OUT_REV);
+    long positions[2];
+    positions[0] = long(x*STEPS_PER_CM);
+    positions[1] = long(y*STEPS_PER_CM);
+    Serial.print("Move x to ");
+    Serial.println(positions[0]);
+    Serial.print("Move y to ");
+    Serial.println(positions[1]);
+    stepperMotorX.moveTo(positions[0]);
+    steppers.moveTo(positions);
+    steppers.runSpeedToPosition();
 }
 
 void loop()
 {
     float x,y;
     receivePosition(&x, &y);
-    float moveX, moveY;
-    computeMove(x, y, &moveX, &moveY);
-    //Move
-    posX = x;
-    posY = y;
-    //Serial.print("X is ");
-    //Serial.println(x);
-    //Serial.print("Y is ");
-    //Serial.println(y);
-    //int stepsRequired = STEPS_PER_OUT_REV;
-    //stepperMotor.setSpeed(800);
-    //stepperMotor.step(stepsRequired);
-    //delay(500);
-    //stepsRequired = -STEPS_PER_OUT_REV;
-    //stepperMotor.setSpeed(700);
-    //stepperMotor.step(stepsRequired);
-    //delay(500);
+    move(x, y);
 }
 
